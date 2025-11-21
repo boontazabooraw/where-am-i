fd9d978ce1d7a9c4ea49788145a5013717aefa98
Leon Bryan <leonard.bryan.email@gmail.com>
2025-11-21 22:35:58 +0800

    First commit

---
+++ src/components/Card.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useLocate } from "../hooks/useLocate";
+import { useTime } from "../hooks/useTime";
+// import ReactCountryFlag from "react-country-flag";
+import { Globe, Loader } from "feather-icons-react";
+
+export default function Card() {
+  const { data, loading } = useLocate();
+  const time = useTime();
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return (
+    <div
+      className="opacity-80 border-b  rounded-xl p-5 shadow-inner shadow-green-500/90 text-shadow-lg text-shadow-green-500/20
+    bg-radial from-green-500/30 to-transparent hover:text-shadow-green-500/30 transition-all duration-700"
+    >
+      <div className="flex flex-col gap-3">
+        <div className="flex flex-row items-center justify-between gap-3">
+          <div>
+            <Globe size={75} />
+          </div>
+          <div className="">
+            <h2 className="text-3xl tracking-wide">Your Ip Address</h2>
+            <h1 className="text-4xl font-black">{data?.query}</h1>
+          </div>
+        </div>
+        <div className="flex flex-row justify-center items-center gap-5">
+          {/* {data && (
+            <ReactCountryFlag
+              countryCode={data.countryCode}
+              style={{
+                fontSize: "2.5em",
+                lineHeight: "2.5em",
+                opacity: 0.8,
+              }}
+              svg
+            />
+          )} */}
+          <h1 className="text-xl">{data?.country}</h1>
+          <h1 className="text-xl">{data?.regionName}</h1>
+          <h1 className="text-xl">{data?.zip}</h1>
+        </div>
+        <h1 className="text-3xl tracking-widest text-center">{time}</h1>
+        <div className="text-center">
+          <h1>{data?.isp}</h1>
+        </div>
+        <div className="text-center text-sm">
+          {data?.proxy ? (
+            <h1 className="text-green-500 text-shadow-lg text-shadow-green-500/20 hover:text-shadow-green-500/40 transition-all duration-400">You are protected.</h1>
+          ) : (
+            <h1 className="text-red-600 text-shadow-lg text-shadow-red-500/20 hover:text-shadow-red-500/40 transition-all duration-400">
+              You are not protected.
+            </h1>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/hooks/useLocate.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+interface DataInterface {
+  country: string;
+  countryCode: string;
+  query: string;
+  city: string;
+  isp: string;
+  regionName: string;
+  zip: string;
+  proxy: boolean;
+}
+
+export function useLocate() {
+  const [data, setData] = useState<DataInterface | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://ip-api.com/json/")
+      .then((res) => {
+        return res.json();
+      })
+      .then((d: DataInterface) => {
+        setData(d);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
+  return {data, loading};
+}
