91e352267e1d98961bda454caa08a8377a31f5d6
Leon Bryan <leonard.bryan.email@gmail.com>
2025-11-21 23:43:45 +0800

    Trying server-side fetch

--- api/ip-api.ts
+++ api/ip-api.ts
@@ -1,5 +1,7 @@
 export default async function handler(req: any, res: any) {
-  const ipAddress = req.headers("x-forwarded-for") || req.socket.remoteAddress;
+  const ipAddress =
+    (req.headers["x-forwarded-for"] as string)?.split(",")[0] ||
+    req.socket?.remoteAddress;
   const response = await fetch(`http://ip-api.com/json/${ipAddress}`);
   const data = await response.json();
   res.status(200).json(data);
